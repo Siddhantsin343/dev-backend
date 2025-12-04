059cf440b2c6154e76af527552af49cd73045e59
siddhant <siddhantsin343@gmail.com>
2025-12-04 21:46:00 +0530

    route method using postman

--- src/app.js
+++ src/app.js
@@ -2,20 +2,26 @@ const express = require("express")
 
 const app = express()
 
-app.use("/home",(req, res)=>{
-    res.send("Home page of server .")
-
+app.get("/user",(req , res)=>{
+    res.send({name : "siddhant", surname : "singh"})
 })
 
+app.post("/user",(req, res)=>{
+    console.log("data is saved in DB");
+    res.send("data succesfully added to DB")
+})
+
+app.delete("/user",(req, res)=>{
+    console.log("data is saved in DB");
+    res.send("data delete to DB")
+})
+
+
+//this will match all the HTTP Method API calls to /hello
 app.use("/hello",(req, res)=>{
     res.send("Hello form the server.")
 
 })
-
-app.use("/test",(req, res)=>{
-    res.send("Hello From Test... 📡")
-})
-
 
 app.listen(3000, ()=>{
     console.log("server is runnning http://localhost:3000/")
