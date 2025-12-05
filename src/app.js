d87af8ca1cb26d5aadee54f461e4f22a479d6d65
siddhant <siddhantsin343@gmail.com>
2025-12-05 21:20:59 +0530

    Multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,26 +2,41 @@ const express = require("express")
 
 const app = express()
 
-app.get("/user",(req , res)=>{
-    res.send({name : "siddhant", surname : "singh"})
+// // Lec- 17 
+// app.get("/user",(req , res)=>{
+//     res.send({name : "siddhant", surname : "singh"})
+// })
+
+// app.post("/user",(req, res)=>{
+//     console.log("data is saved in DB");
+//     res.send("data succesfully added to DB")
+// })
+
+// app.delete("/user",(req, res)=>{
+//     console.log("data is saved in DB");
+//     res.send("data delete to DB")
+// })
+
+
+// //this will match all the HTTP Method API calls to /hello
+// app.use("/hello",(req, res)=>{
+//     res.send("Hello form the server.")
+
+// })
+
+
+// Lec 18 middleware & errorHandling
+ 
+app.use("/user", (req, res, next)=>{
+    console.log("handle the route user 1");
+    res.send("Response!!!")
+    next()
+},(req , res)=>{
+    console.log("handle the route user 1");
+    res.send("2nd Response")
 })
 
-app.post("/user",(req, res)=>{
-    console.log("data is saved in DB");
-    res.send("data succesfully added to DB")
-})
 
-app.delete("/user",(req, res)=>{
-    console.log("data is saved in DB");
-    res.send("data delete to DB")
-})
-
-
-//this will match all the HTTP Method API calls to /hello
-app.use("/hello",(req, res)=>{
-    res.send("Hello form the server.")
-
-})
 
 app.listen(3000, ()=>{
     console.log("server is runnning http://localhost:3000/")
